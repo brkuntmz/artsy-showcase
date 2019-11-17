1eda3a92c4572b91fa02ccf0fee26874145d9837
Burak Unutmaz <burak.unutmaz@youncta.com>
2019-11-17 16:50:15 +0100

    Updates

---
+++ precache-manifest.29ff3205cbb6793822e5e0b9678fd3a3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68dc4ddc545ca277c193a2e3634c3b2f",
+    "url": "/artsy-showcase/index.html"
+  },
+  {
+    "revision": "e78ceaed6dc4fcd4ff7e",
+    "url": "/artsy-showcase/static/css/main.bf2b6c14.chunk.css"
+  },
+  {
+    "revision": "42741bccd3a387c3a324",
+    "url": "/artsy-showcase/static/js/2.f18e2b7f.chunk.js"
+  },
+  {
+    "revision": "e78ceaed6dc4fcd4ff7e",
+    "url": "/artsy-showcase/static/js/main.ad202886.chunk.js"
+  },
+  {
+    "revision": "ad32fe20c4da6af423d1",
+    "url": "/artsy-showcase/static/js/runtime~main.c9fa8fe3.js"
+  },
+  {
+    "revision": "2c97f487490522af92c353dcfca52e3c",
+    "url": "/artsy-showcase/static/media/clear.2c97f487.svg"
+  },
+  {
+    "revision": "6eca94c3a83e6b97759f31663fc6bded",
+    "url": "/artsy-showcase/static/media/loading-indicator.6eca94c3.svg"
+  },
+  {
+    "revision": "a8b3079ef10f6996045572ed05a41dec",
+    "url": "/artsy-showcase/static/media/magnifying-glass.a8b3079e.svg"
+  },
+  {
+    "revision": "29a98b0bd1a3e8875969af5e1101c6f7",
+    "url": "/artsy-showcase/static/media/photo.29a98b0b.svg"
+  }
+]);
